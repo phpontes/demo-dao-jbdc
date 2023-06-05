400046de57c855c7871fc4f220fbe10b8aa67cc0
Paulo Pontes <ppontes95@hotmail.com>
2023-06-05 10:45:14 -0300

    deletion implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,6 +12,8 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -41,5 +44,13 @@ public class Program {
 		seller.setName("Martha White");
 		sellerDao.update(seller);
 		System.out.println("Update successful");
+		
+		System.out.println("\n==== TEST 6: seller delete ====");
+		System.out.println("Enter id for deletion test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Deletion complete");
+		
+		sc.close();
 	}
 }
